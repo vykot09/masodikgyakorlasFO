027420b7e8c13ec0baa97f24b9d475a9f19fc696
vykot09 <viktorbajari99@gmai.com>
2019-12-16 13:02:38 +0100

    ismet

--- masodikFeladat/masodikFeladat/Program.cs
+++ masodikFeladat/masodikFeladat/Program.cs
@@ -14,9 +14,15 @@ namespace masodikFeladat
             int magassag = Console.WindowHeight;
             //Console.WriteLine($"{szelesseg} és {magassag}");
 
+            Random rnd = new Random();
+
             int x = szelesseg / 2;
             int y = magassag / 2;
 
+            int[] pontokX = new int[10];
+            int[] pontokY = new int[10];
+
+           
             int[] xKoord = new int[5];
             int[] yKoord = new int[5];
 
@@ -24,6 +30,12 @@ namespace masodikFeladat
             {
                 xKoord[i] = x + i;
                 yKoord[i] = y;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                pontokX[i] = rnd.Next(0, szelesseg);
+                pontokY[i] = rnd.Next(0, magassag);
             }
 
             Megrajzol(xKoord, yKoord);
@@ -39,21 +51,54 @@ namespace masodikFeladat
                         Console.Clear();
                         Balfele(ref xKoord, ref yKoord);
                         Megrajzol(xKoord, yKoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xKoord[xKoord.Length - 1] && pontokY[i] == yKoord[yKoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
+
                         break;
                     case ConsoleKey.UpArrow:
                         Console.Clear();
                         Felfele(ref xKoord, ref yKoord);
                         Megrajzol(xKoord, yKoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xKoord[xKoord.Length - 1] && pontokY[i] == yKoord[yKoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                     case ConsoleKey.RightArrow:
                         Console.Clear();
                         Jobbfele(ref xKoord, ref yKoord);
                         Megrajzol(xKoord, yKoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xKoord[xKoord.Length - 1] && pontokY[i] == yKoord[yKoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                     case ConsoleKey.DownArrow:
                         Console.Clear();
                         Lefele(ref xKoord, ref yKoord);
                         Megrajzol(xKoord, yKoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xKoord[xKoord.Length - 1] && pontokY[i] == yKoord[yKoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                 }
             }
@@ -63,7 +108,7 @@ namespace masodikFeladat
 
         static void Megrajzol(int[] x, int[] y)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < x.Length; i++)
             {
                 Console.SetCursorPosition(x[i], y[i]);
                 Console.Write('*');
@@ -120,6 +165,24 @@ namespace masodikFeladat
             }
             x[4] = atmenetX[4] - 1;
             y[4] = atmenetY[4];
+        }
+        static int[] EggyelCsokkent(int szam, int[] a)
+        {
+            int[] b = new int[a.Length - 1];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (szam != a[i])
+                {
+                     b[i] = a[i];
+                }
+                else if(szam == a[i] && i != a.Length -1)
+                {
+                    b[i++] = a[i + 1];
+                }
+            }
+
+            return b;
         }
     }
 }
